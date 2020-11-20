48404fe9e3421361db89582a3bdc75aad649d4ef
Joe Maniaci <joe.maniaci1@gmail.com>
2020-11-19 21:15:42 -0500

    set local storage and created function to render the last search upon refreshing page

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
+//displays last search result upon reloading
+function setInitialDisplay() {
+
+  if (!localStorage.getItem("searchedCity")) {
+    breweryCall("Cleveland");
+   // hotelSearch("Cleveland");
+  } else {
+    breweryCall(localStorage.getItem("searchedCity"));
+   // hotelSearch(localStorage.getItem("searchedCity"));
+  }
+}
+ setInitialDisplay();
+
 //Brewery API call
 function breweryCall() {
 
   var cityName = "cleveland";
   var breweryType = "brewpub"
   var breweryURL = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
-  
-  if(breweryType) {
-    breweryURL = breweryURL + "&by_type=" + breweryType; 
+
+  //Updates the breweryURL if a breweryType was selected
+  if (breweryType) {
+    breweryURL = breweryURL + "&by_type=" + breweryType;
   }
 
+  $.ajax({
 
-$.ajax({
+    url: breweryURL,
 
-url: breweryURL,
+    method: "GET"
 
-method: "GET"
+  }).then(function (response) {
 
-}).then(function(response) {
+    console.log(response);
+    //created variables for the inforamtion we want to display
+    var breweryName = response[0].name;
+    var breweryAddress = response[0].street;
+    var breweryWebsite = response[0].website_url;
+    
+    //Grabs the HTML element that the api information will be appended too
+    mainDiv = $(".mainDiv");
 
-  console.log(response);
+    //clear previous search results
+    mainDiv.empty();
+    
+    //create html elements to display information
+    createdDiv = $("<div>").attr("class", "brewery");
+    createdH2 = $("<h2>" + breweryName + "<h2>").attr("class", "breweryName");
+    createdP1 = $("<p> Address: " + breweryAddress + "<p>").attr("class", "breweryAddress");
+    createdP2 = $("<p>Website: <a href=" + breweryWebsite + ">" + breweryWebsite + "</a></p>").attr("class", "breweryWebsite");
 
-  //created variables for the inforamtion we want to display
-  var breweryName = response[0].name;
-  var breweryAddress = response[0].street;
-  var breweryWebsite = response[0].website_url;
-  
-  //create html elements to display information
-  mainDiv = $(".mainDiv");
-  
-  createdDiv = $("<div>").attr("class", "brewery");
-  createdH2 = $("<h2>" + breweryName + "<h2>").attr("class", "breweryName");
-  createdP1 = $("<p>" + breweryAddress + "<p>").attr("class", "breweryAddress");
-  createdP2 = $("<a href=" + breweryWebsite + ">" + breweryWebsite + "</a>").attr("class", "breweryWebsite");
+    //appends created HTML elements
+    mainDiv.append(createdDiv, createdH2, "<hr>", createdP1, createdP2);
 
-  mainDiv.append(createdDiv, createdH2, createdP1, createdP2);
-
+    //set localStorage
+    localStorage.setItem("searchedCity", cityName);
   })
 }
 
@@ -42,10 +61,10 @@ breweryCall();
 
 //add click event to search button
 //calls the API functions
-$(".searchButton").on("click", function(event) {
+$(".searchButton").on("click", function (event) {
 
   event.preventDefault;
 
   breweryCall();
-  //Input hotel API call
+  hotelSearch(cityName);
 });
